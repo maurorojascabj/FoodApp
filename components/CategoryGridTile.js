98c35475a4c16708263f11d4fcb20f852116bbe3
Mauro Rojas <maurorojascabj@gmail.com>
2020-09-16 19:27:38 -0300

    Se creo el componente para el formato de los titulos de las comidas que pertenecen a una categoria seleccionada previamente.

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,38 +1,62 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, ImageBackground} from 'react-native';
 
 const CategoryGridTile = props => {
-    return(
-        <TouchableOpacity
-            style={{...styles.gridItem, ...{backgroundColor:props.color}}}
-            onPress={props.onSelect}
-        >
-            <View>
-                <Text>{props.title}</Text>
-            </View>
-        </TouchableOpacity>
-    );
+	const image = {uri: props.image}
+	return(
+		<TouchableOpacity
+			style={{...styles.gridItem}}
+			onPress={props.onSelect}
+		>	 
+			<ImageBackground
+				source={image}
+				style={styles.image}
+				imageStyle={styles.imageStyles}
+			>	
+				<View style={styles.titleContainer}>
+					<Text style={styles.title}>{props.title}</Text>			
+				</View>
+			</ImageBackground>	
+		
+		</TouchableOpacity>
+	);
 };
 
 const styles = StyleSheet.create({
-    gridItem:{
-        flex: 1,
-        margin: 15,
-        height: 150,
-        justifyContent: 'center',
-        alignItems: 'center',
-        //iOS
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 2},
-        shadowOpacity: 0.26,
-        shadowRadius: 6,
-        //
-        elevation: 5, //Android
-        backgroundColor:'white',
-        padding: 20,
-        borderRadius: 20,
+	gridItem:{
+		flex: 1,
+		margin: 15,
+		height: 150,
+		justifyContent: 'center',
+		alignItems: 'center',
+		//iOS
+		shadowColor: 'black',
+		shadowOffset: {width: 0, height: 2},
+		shadowOpacity: 0.26,
+		shadowRadius: 6,
+		//
+		elevation: 5, //Android
+		backgroundColor:'white',
+		borderRadius: 20,
+	},
+	titleContainer:{
+		flex:1,
+		justifyContent:'center',
+		alignItems:'center',
+	},
+	title:{
+		color: 'white',
+		backgroundColor: 'rgba(0,0,0,0.4)',
+		fontSize: 15,
+	},
+	image:{
+		width:'100%',
+		height:'100%',
+	},
+	imageStyles:{
+		borderRadius: 20
+	},
 
-    },
 });
 
 export default CategoryGridTile;
