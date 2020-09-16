eb7d05bf89890efa2465e78efaf613f53ae41856
Mauro Rojas <maurorojascabj@gmail.com>
2020-09-16 19:19:44 -0300

    Se creo la pantalla de Categorias.Luego se agregaron imagenes de fondo para representar para categoria

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,47 +1,49 @@
 //Pantalla para seleccionar una categoría de comida
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, FlatList, Image} from 'react-native';
+import {FlatList, StyleSheet} from 'react-native';
 
 import {CATEGORIES} from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
 
 const CategoriesScreen = props => {
-    const renderGridItem = itemData => {
-        return(
-            <CategoryGridTile 
-                title={itemData.item.title}
-                color={itemData.item.color}
-                onSelect={()=>{
-                    props.navigation.navigate({
-                        routeName: 'CategoryMeals',
-                        params: { //Envían parametros a la pantalla que apunta routeName
-                            categoryId: itemData.item.id,      
-                        },
-                    });
-                }}
-            />
-        );
-    };
-    return (
-        <FlatList 
-            keyExtractor={(item, index) => item.id}
-            data={CATEGORIES}
-            renderItem={renderGridItem}
-            numColumns={2} />
-    );
+	const renderGridItem = itemData => {
+		return(
+			<CategoryGridTile 
+				title={itemData.item.title}
+				color={itemData.item.color}
+				image={itemData.item.image}
+				onSelect={()=>{
+					props.navigation.navigate({
+						routeName: 'CategoryMeals',
+						params: { //Envían parametros a la pantalla que apunta routeName
+								categoryId: itemData.item.id,      
+						},
+					});
+				}}
+			/>
+		);
+	};
+	return (
+		<FlatList 
+			keyExtractor={(item, index) => item.id}
+			data={CATEGORIES}
+			renderItem={renderGridItem}
+			numColumns={2}
+		/>
+	);
 };
 
 CategoriesScreen.navigationOptions = {
-    headerTitle: 'Categorías',
+	headerTitle: 'Categorías',
 };
 
 const styles = StyleSheet.create({
-    screen:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: "center",
-    },
+	screen:{
+			flex: 1,
+			justifyContent: 'center',
+			alignItems: "center",
+	},
 });
 
 export default CategoriesScreen;
