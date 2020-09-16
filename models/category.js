39614676cadc645731e4d675eecc474dd64df6f8
Mauro Rojas <maurorojascabj@gmail.com>
2020-09-16 19:25:51 -0300

    Se agrego el atributo imagen, para cada categoria a usarse en la pantalla correspondiente

--- models/category.js
+++ models/category.js
@@ -1,9 +1,10 @@
 //Definimos el modelo de los datos con los que trabajaremos
 class Category{
-    constructor(id, title, color){ //Color para usar en la cuadrícula propia para una categoria
+    constructor(id, title, color, image){ //Color para usar en la cuadrícula propia para una categoria
         this.id = id;
         this.title = title;
         this.color = color;
+        this.image = image;
     };
 };
 
